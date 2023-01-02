ead5c8256214928fa65f65958a8b528fad3f6d64
cirko33 <luka.ciric2106@gmail.com>
2023-01-02 19:07:37 +0100

    Making certificates

---
+++ Project/LocalDB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
