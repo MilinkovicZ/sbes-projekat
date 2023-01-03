4c81a2b0751d03fdcd768f16389216701408f950
cirko33 <luka.ciric2106@gmail.com>
2023-01-03 13:34:40 +0100

    Adding model and database model

---
+++ Project/CentralDB/DataBase/DataBase.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDB
+{
+    public class DataBase
+    {
+        static string path = AppDomain.CurrentDomain.BaseDirectory + "DataBase/data.json";
+        public static List<Expense> GetExpenses()
+        {
+            var list = JsonConvert.DeserializeObject<List<Expense>>(File.ReadAllText(path));
+            return list == null ? new List<Expense>() : list;
+        }
+
+        public static List<Expense> GetExpenses(List<string> regions)
+        {
+            return GetExpenses().FindAll(t => regions.Contains(t.Region));
+        }
+
+        public static Expense GetExpense(string id)
+        {
+            return GetExpenses().Find(t => t.Id == id);
+        } 
+
+        public static void Add(Expense expense)
+        {
+            var list = GetExpenses();
+            expense.Id = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            list.Add(expense);
+            File.WriteAllText(path, JsonConvert.SerializeObject(list));
+        }
+
+        public static void Update(Expense expense)
+        {
+            var list = GetExpenses();
+            int ind = list.FindIndex(t => t.Id == expense.Id);
+            if(ind != -1)
+            {
+                list[ind] = expense;
+                File.WriteAllText(path, JsonConvert.SerializeObject(list));
+            }
+        }
+
+        public static void Delete(string id)
+        {
+            var list = GetExpenses();
+            int ind = list.FindIndex(t => t.Id == id);
+            if(ind != -1)
+            {
+                list.RemoveAt(ind);
+                File.WriteAllText(path, JsonConvert.SerializeObject(list));
+            }
+        }
+
+        public static void UpdateAll(List<Expense> expenses)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(expenses));
+        }
+    }
+}
---
+++ Project/Common/Expense.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Expense
+    {
+        string id;
+        string region;
+        string city;
+        uint year;
+        Dictionary<string, double> expensesPerMonth;
+
+        public Expense()
+        {
+            ExpensesPerMonth = new Dictionary<string, double>();
+        }
+        public Expense(string id, string region, string city, uint year, Dictionary<string, double> expensesPerMonth)
+        {
+            Id = id;
+            Region = region;
+            City = city;
+            Year = year;
+            ExpensesPerMonth = expensesPerMonth;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Region { get => region; set => region = value; }
+        public string City { get => city; set => city = value; }
+        public uint Year { get => year; set => year = value; }
+        public Dictionary<string, double> ExpensesPerMonth { get => expensesPerMonth; set => expensesPerMonth = value; }
+    }
+}
