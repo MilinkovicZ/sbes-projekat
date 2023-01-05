2f5a01cbaf8ba9608036c644c93e1f989088e623
cirko33 <luka.ciric2106@gmail.com>
2023-01-06 00:01:19 +0100

    Fixing key

--- Project/Common/Cryptography/SecretKey.cs
+++ Project/Common/Cryptography/SecretKey.cs
@@ -8,53 +8,17 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-	public class SecretKey
-	{
-		public static string GenerateKey()
-		{
-			SymmetricAlgorithm symmAlgorithm = AesCryptoServiceProvider.Create();
-
-			return ASCIIEncoding.ASCII.GetString(symmAlgorithm.Key);
-		}
-		
-		public static void StoreKey(string secretKey, string outFile)
-		{
-			FileStream fOutput = new FileStream(outFile, FileMode.OpenOrCreate, FileAccess.Write);
-			byte[] buffer = Encoding.ASCII.GetBytes(secretKey);
-
-			try
-			{
-				fOutput.Write(buffer, 0, buffer.Length);
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("SecretKeys.StoreKey:: ERROR {0}", e.Message);
-			}
-			finally
-			{
-				fOutput.Close();
-			}
-		}
-
-		public static string LoadKey(string inFile)
-		{
-			FileStream fInput = new FileStream(inFile, FileMode.Open, FileAccess.Read);
-			byte[] buffer = new byte[(int)fInput.Length];
-
-			try
-			{
-				fInput.Read(buffer, 0, (int)fInput.Length);
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("SecretKeys.LoadKey:: ERROR {0}", e.Message);
-			}
-			finally
-			{
-				fInput.Close();
-			}
-
-			return ASCIIEncoding.ASCII.GetString(buffer);
-		}
-	}
+    public class SecretKey
+    {
+        public static string GenerateKey(string identity)
+        {
+            string _pepper = "secret";
+            using (var sha = SHA256.Create())
+            {
+                var computedHash = sha.ComputeHash(
+                Encoding.Unicode.GetBytes(identity + _pepper));
+                return Convert.ToBase64String(computedHash);
+            }
+        }
+    }
 }
