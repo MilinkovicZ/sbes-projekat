8b1871278623b79f629548e7b80172eeb1d03569
cirko33 <luka.ciric2106@gmail.com>
2023-01-05 16:26:05 +0100

    Added TODO files

---
+++ Project/Client/WCFClient.cs
@@ -0,0 +1,15 @@
+﻿using LDBContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class WCFClient : ChannelFactory<ILocalService>, ILocalService, IDisposable
+    {
+        //TO DO - Decrypt ans and encrypt req
+    }
+}
---
+++ Project/LocalDB/WCFService.cs
@@ -0,0 +1,14 @@
+﻿using LDBContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDB
+{
+    class WCFService : ILocalService
+    {
+        //TO DO - Decrypt req and encrypt ans
+    }
+}
